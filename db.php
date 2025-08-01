4fa3bcd596528e3214f21ebabbe4fda151c8cc18
Harshrajsinh Zala <harshrajsinhz1684@gmail.com>
2025-08-01 19:24:09 +0530

    change

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT') ?: 5432;
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
+$host = "localhost";
+$port = "5432"; 
+$dbname = "traveltales";
+$user = "postgres";
+$pass = ""; 
 
 try {
     $pdo = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require;sslrootcert=/etc/ssl/certs/ca-certificates.crt;options='--client_encoding=UTF8'",
+        "pgsql:host=$host;port=$port;dbname=$dbname;options='--client_encoding=UTF8'",
         $user,
         $pass,
         [
